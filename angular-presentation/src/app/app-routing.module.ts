bfddc985bcbc862eadd79f0830413b3d4280ddf1
Niklas Geppert <Niklas.Geppert@cas.de>
2023-04-25 20:02:28 +0200

    Adding first Component

--- angular-presentation/src/app/app-routing.module.ts
+++ angular-presentation/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StartPageComponent } from './start-page/start-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path:'', component:StartPageComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
